e7455032e9bb8cb07f3be45c6e15855bab23ca30
AmitabhDey-Byte <amitabhdey101@gmail.com>
2025-12-16 18:43:42 +0530

    stopwatch done although aint styled

--- stopwatch/src/Stopwatch.jsx
+++ stopwatch/src/Stopwatch.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-function Stopwatch()
+function stopwatch()
 {
 const[isRunning, setisRunning] = useState(false);
 const[elapsedtime, setelapsedtime] = useState(0);
@@ -20,6 +20,41 @@ useEffect(() =>
         clearInterval(intervalidref.current);
     }
 },[isRunning]);
-
+function start()
+{
+setisRunning(true);
+starttimeref.current=Date.now()-elapsedtime;
+}
+function stop()
+{
+setisRunning(false);
+}
+function reset()
+{
+    elapsedtime(0);
+    setisRunning(false);
+}
+function format()
+{
+    let hour = Math.floor(elapsedtime/(1000*60*60));
+    let min = Math.floor(elapsedtime/(1000*60));
+    let sec = Math.floor((elapsedtime/1000)%60);
+    let mili = Math.floor((elapsedtime%10000)/10);
+    hour = String(hour).padStart(2, "0");
+    min = String(min).padStart(2, "0");
+    sec = String(sec).padStart(2, "0");
+    mili = String(mili).padStart(2, "0");
+return '${min}:${sec}:${mili}';
+}
+return(
+    <div className = "Stopwatch">
+        <div className="Display">{format}</div>
+        <div className="Controls">
+            <button className="start-button" onClick ={start}>Start</button>
+            <button className="start-button" onClick ={stop}></button>
+            <button className="start-button" onClick ={reset}></button>
+                    </div>
+    </div>
+);
  }
-export default Stopwatch
+export default stopwatch
